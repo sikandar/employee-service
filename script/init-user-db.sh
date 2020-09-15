fc5fd4eb276dc361f7317c7c4b3ff66e4c8e78fd
Sikandar Ali Awan <mysialkot@gmail.com>
2020-09-15 22:02:37 +0200

    Delete init-user-db.sh

--- script/init-user-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE postgres; 
-    ALTER SYSTEM SET max_connections TO '1000';
-    select pg_reload_conf();
-EOSQL
